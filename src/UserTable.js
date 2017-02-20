57bcd3b57348a94e7dba8ad9207a75a639aa86ea
Scott Woodend <woodendscott@gmail.com>
2017-02-20 14:09:16 -0500

    Create a UserTable Component. This component will make the api call and sort the data based on the user selected preferences and then display it in a table format

--- src/UserTable.js
+++ src/UserTable.js
@@ -1,9 +1,87 @@
 import React, { Component } from 'react';
 
+const apiEndpoint = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent'
+
 class UserTable extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      sortedByRecent: [],
+      sortedByAllTime: []
+    };
+  }
+
+  componentDidMount(){
+    fetch(apiEndpoint).then((response) =>{
+      return response.json()
+    }).then((fccData) =>{
+      let fccDataCopy = fccData.slice();
+      let sortedByAllTime = this.sortByAllTime(fccData);
+      let sortedByRecent = this.sortByRecent(fccDataCopy);
+
+      this.setState({
+        sortedByRecent,
+        sortedByAllTime
+      });
+    });
+  };
+
+  sortByRecent(data){
+    return data.sort((a,b) =>{
+      if(a.recent > b.recent){
+        return -1;
+      } else if (b.recent > a.recent){
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+  }
+
+  sortByAllTime(data){
+    return data.sort((a, b) =>{
+      if(a.alltime > b.alltime){
+        return -1;
+      } else if (b.alltime > a.alltime){
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+
   render(){
+    //this.props.selectedAllTime
+    let data = this.props.selectedAllTime ? this.state.sortedByAllTime : this.state.sortedByRecent
+    console.log('data is', data);
+    let tableData = data.map((user, i) =>{
+      return (
+        <tr>
+          <td>{i+1}</td>
+          <td>{user.username}</td>
+          <td>{user.alltime}</td>
+          <td>{user.recent}</td>
+        </tr>
+      );
+    });
     return (
-      <div className="user-table">hi</div>
+      <div className="user-table">
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Username</th>
+              <th>AllTime</th>
+              <th>Recent</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
