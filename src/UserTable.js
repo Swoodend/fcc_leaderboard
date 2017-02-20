14f935d44057756aa2212b93cabe9a5b5957878c
Scott Woodend <woodendscott@gmail.com>
2017-02-20 02:06:45 -0500

    check table position

---
+++ src/UserTable.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class UserTable extends Component{
+  render(){
+    return (
+      <div className="user-table">hi</div>
+    );
+  }
+}
+
+export default UserTable;
