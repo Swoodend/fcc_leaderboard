a4d98ce9b23b6b06a971276f1b7088d618e9d144
Scott Woodend <woodendscott@gmail.com>
2017-02-20 02:05:55 -0500

    SortAllTime will update NavBar parent

---
+++ src/SortAllTime.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class SortAllTime extends Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.props.sortAllTimeClicked();
+  }
+
+  render(){
+
+    let activeStyle = {
+      background : 'white',
+      color: '#006400'
+    };
+
+    if (this.props.active){
+      return <div onClick={this.handleClick} style={activeStyle} className="nav-item">Sort by All Time</div>
+    } else {
+      return <div onClick={this.handleClick}className="nav-item">Sort by All Time</div>
+    }
+  }
+}
+
+export default SortAllTime;
